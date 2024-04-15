498d82fc98f2f1e29027b0477e0259352fa223b0
Diego Castro <diego@Diegos-MacBook-Air.local>
2024-04-15 18:04:20 -0300

    TODOS-SERVER: Added ts-node

--- src/models/todoModel.ts
+++ src/models/todoModel.ts
@@ -1,15 +1,15 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-interface ITodo extends Document {
+interface Todo extends Document {
   title: string;
   completed: boolean;
 }
 
-const todoSchema = new Schema<ITodo>({
+const todoSchema = new Schema<Todo>({
   title: { type: String, required: true },
   completed: { type: Boolean, default: false },
 });
 
-const Todo = mongoose.model<ITodo>("Todo", todoSchema);
+const TodoModel = mongoose.model<Todo>("Todo", todoSchema);
 
-export default Todo;
+export default TodoModel;
